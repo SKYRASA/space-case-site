0d7dba633e3bd2514a88f221ea6fdb08a7887046
SKYRASA <taranenkodavid1@gmail.com>
2026-03-15 16:17:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,88 @@
 let skins = [
 
-{name:"Common", class:"common"},
-{name:"Common", class:"common"},
-{name:"Rare", class:"rare"},
-{name:"Rare", class:"rare"},
-{name:"Epic", class:"epic"},
-{name:"Legendary", class:"legendary"}
+{
+name:"Space Pistol",
+img:"https://i.imgur.com/9QO4F9V.png",
+rarity:"common",
+chance:50
+},
+
+{
+name:"Galaxy Rifle",
+img:"https://i.imgur.com/XzQZ6QG.png",
+rarity:"rare",
+chance:30
+},
+
+{
+name:"Nebula Sniper",
+img:"https://i.imgur.com/v9y6F6T.png",
+rarity:"epic",
+chance:15
+},
+
+{
+name:"Cosmic Dragon",
+img:"https://i.imgur.com/VuK6C6H.png",
+rarity:"legendary",
+chance:5
+}
 
 ]
+
+function getRandomSkin(){
+
+let rand = Math.random()*100
+let sum = 0
+
+for(let skin of skins){
+
+sum += skin.chance
+
+if(rand <= sum){
+
+return skin
+
+}
+
+}
+
+}
 
 function openCase(){
 
 let itemsDiv = document.getElementById("items")
+
 itemsDiv.innerHTML=""
 
-let roll = []
+let roll=[]
 
 for(let i=0;i<40;i++){
 
-let skin = skins[Math.floor(Math.random()*skins.length)]
+let skin=getRandomSkin()
 
 roll.push(skin)
 
-let div = document.createElement("div")
+let div=document.createElement("div")
 
-div.className="item "+skin.class
-div.innerText=skin.name
+div.className="item "+skin.rarity
+
+div.innerHTML=
+"<img src='"+skin.img+"'><p>"+skin.name+"</p>"
 
 itemsDiv.appendChild(div)
 
 }
 
-let win = roll[35]
+let win=roll[35]
 
 setTimeout(()=>{
 
-document.getElementById("result").innerText="YOU GOT: "+win.name
+document.getElementById("result").innerHTML=
+"🎉 YOU GOT: "+win.name
 
-},4000)
+},5000)
 
-itemsDiv.style.transform="translateX(-4200px)"
+itemsDiv.style.transform="translateX(-4900px)"
 
 }
